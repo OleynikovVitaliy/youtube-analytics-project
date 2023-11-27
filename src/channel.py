afac0a082fa6c223d34d4e26880839f88fce033f
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2023-11-27 20:23:28 +0500

    homework-2 modification

--- src/channel.py
+++ src/channel.py
@@ -8,12 +8,12 @@ class Channel:
 
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
-    channel_id = 'UCwHL6WHUarjGfUM_586me8w'
-    dict_to_print = []
+#    channel_id = 'UCwHL6WHUarjGfUM_586me8w'
+#    dict_to_print = []
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
@@ -21,6 +21,10 @@ class Channel:
         self.subscriberCount = self.channel['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.viewCount = self.channel['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
