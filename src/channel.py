3d6cf8cb4e12dda904822c06834bc691b988d603
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2023-11-29 21:30:27 +0500

    homework-3

--- src/channel.py
+++ src/channel.py
@@ -8,8 +8,6 @@ class Channel:
 
     api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
-#    channel_id = 'UCwHL6WHUarjGfUM_586me8w'
-#    dict_to_print = []
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -21,6 +19,38 @@ class Channel:
         self.subscriberCount = self.channel['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.viewCount = self.channel['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        """Метод возвращающий название и ссылку на канал"""
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """Метод добавления"""
+        return self.subscriberCount + other.subscriberCount
+
+    def __sub__(self, other):
+        """Метод вычитания"""
+        return int(self.subscriberCount) - int(other.subscriberCount)
+
+    def __gt__(self, other):
+        """Метод сравнения (больше)"""
+        return int(self.subscriberCount) > int(other.subscriberCount)
+
+    def __ge__(self, other):
+        """Метод сравнения (дольше или равно)"""
+        return int(self.subscriberCount) >= int(other.subscriberCount)
+
+    def __lt__(self, other):
+        """Метод сравнения (меньше)"""
+        return int(self.subscriberCount) < int(other.subscriberCount)
+
+    def __le__(self, other):
+        """Метод сравнения (меньше или равно)"""
+        return int(self.subscriberCount) <= int(other.subscriberCount)
+
+    def __eq__(self, other):
+        """Метод сравнения (меньше)"""
+        return int(self.subscriberCount) == int(other.subscriberCount)
 
     @property
     def channel_id(self):
@@ -46,4 +76,4 @@ class Channel:
                        "subscriberCount": self.subscriberCount,
                        "video_count": self.video_count,
                        "viewCount": self.viewCount}, write_file, indent=2, ensure_ascii=False, separators=(',', ': '))
-            print(dict_to_print)
+
