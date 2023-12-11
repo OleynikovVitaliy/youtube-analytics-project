53cc32b7ef26976d4bd385872245668368233d0b
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2023-12-11 21:47:58 +0500

    homework-5 youtube-analytics (Finish)

--- src/playlist.py
+++ src/playlist.py
@@ -3,47 +3,55 @@ import os
 
 import isodate
 from googleapiclient.discovery import build
+from isodate import parse_duration
 
 from helper.youtube_api_manual import youtube
-from src.video import Video
 
 
-class PlayList(Video):
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
-
-    def __init__(self, playlist_id: str, video_info: str):
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        super().__init__(video_info)
+class PlayList:
+    def __init__(self, playlist_id):
         self.playlist_id = playlist_id
-        self.playlist = self.youtube.playlists().list(part='snippet', id=playlist_id).execute()
-        self.title = self.playlist['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/playlist?list={playlist_id}"
+        self.api_key = os.getenv('YT_API_KEY')
+        self.youtube = self.build_youtube_service()
+        self.playlist_info = self.get_playlist_info()
+        self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
+
+    def build_youtube_service(self):
+        return build('youtube', 'v3', developerKey=self.api_key)
+
+    def get_playlist_info(self):
+        request = self.youtube.playlists().list(part="snippet", id=self.playlist_id)
+        response = request.execute()
+        return response['items'][0]['snippet']
+
+    @property
+    def title(self):
+        return self.playlist_info['title']
 
     @property
     def total_duration(self):
-        """
-        Возвращает объект класса `datetime.timedelta` с суммарной длительность плейлиста
-        """
-        playlist_videos = youtube.playlistItems().list(playlistId=self.playlist_id, part='contentDetails',
-                                                       maxResults=50).execute()
+        """возвращает объект класса `datetime.timedelta` с суммарной длительность плейлиста"""
+        videos = self.youtube.playlistItems().list(part="contentDetails",
+                                                   playlistId=self.playlist_id).execute()['items']
+        total_duration = datetime.timedelta()
+        for video in videos:
+            video_id = video["contentDetails"]["videoId"]
+            video_response = self.youtube.videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=video_id).execute()
+            content_details = video_response['items'][0]['contentDetails']
+            duration = content_details.get('duration', '')
+            if duration:
+                parsed_duration = parse_duration(duration)
+                total_duration += parsed_duration
+        return total_duration
 
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
-        video_response = youtube.videos().list(part='contentDetails,statistics', id=','.join(video_ids)).execute()
+    def get_video_ids(self):
+        videos = self.youtube.playlistItems().list(part="contentDetails",
+                                                   playlistId=self.playlist_id).execute()['items']
+        return [video["contentDetails"]["videoId"] for video in videos]
 
-        total_video_duration = datetime.timedelta(hours=0, minutes=0, seconds=0)
-
-        for video in video_response['items']:
-            iso_8601_duration = video['contentDetails']['duration']
-            duration = isodate.parse_duration(iso_8601_duration)
-
-            duration_split = str(duration).split(':')
-            duration = datetime.timedelta(hours=int(duration_split[0]), minutes=int(duration_split[1]),
-                                          seconds=int(duration_split[2]))
-            total_video_duration += duration
-
-        return total_video_duration
-
+    @property
     def show_best_video(self):
         """
         Возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)
